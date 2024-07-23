f814f2cb13779ba03464517b8d04d45a5498557d
ByteSquire <56401181+ByteSquire@users.noreply.github.com>
2024-07-23 18:12:33 +0200

    feat: add swauroray

--- build_files/branding.sh
+++ build_files/branding.sh
@@ -10,6 +10,11 @@ elif test "$BASE_IMAGE_NAME" = "kinoite"; then
     sed -i 's/Bluefin/Aurora/g' /usr/etc/yafti.yml
     sed -i 's/Aurora (Beta)/Aurora \- Bluefin\-KDE (Alpha)/' /usr/etc/yafti.yml
     sed -i 's/Bluefin/Aurora/' /usr/libexec/ublue-flatpak-manager
+elif test "$BASE_IMAGE_NAME" = "sericea"; then
+    sed -i '/^PRETTY_NAME/s/Sericea/Swauroray/' /usr/lib/os-release
+    sed -i 's/Bluefin/Swauroray/g' /usr/etc/yafti.yml
+    sed -i 's/Swauroray (Beta)/Swauroray \- Bluefin\-Sway (Alpha)/' /usr/etc/yafti.yml
+    sed -i 's/Bluefin/Swauroray/' /usr/libexec/ublue-flatpak-manager
 fi
 
 # Watermark for Plymouth
